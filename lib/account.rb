5c7fda914d78731f6122dc04abe6821727b5c64c
pyan83 <puyanwei@gmail.com>
2017-10-02 17:50:56 +0100

    added current transaction method plus test

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,6 @@
 class Account
   DEFAULT_BALANCE = 0
 
-
   attr_accessor :balance, :transaction_history
 
   def initialize
@@ -9,13 +8,20 @@ class Account
     @transaction_history = []
     end
 
-    def deposit(credit, date)
-      @balance += credit
-      @transaction_history << info = Info.new(date, credit, 0)
-    end
+  def deposit(credit, date)
+    @balance += credit
+    current_transaction(date, credit, 0)
+  end
 
-    def withdrawl(debit, date)
-      @balance -= debit
-      @transaction_history << info = Info.new(date, 0, debit)
-    end
+  def withdrawl(debit, date)
+    @balance -= debit
+    current_transaction(date, 0, debit)
+  end
+
+  private
+
+  def current_transaction(date, credit, debit)
+    current_transaction = Info.new(date, credit, debit)
+    @transaction_history << current_transaction
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -11,34 +11,43 @@ RSpec.describe Account do
 
   context '#deposit' do
     it 'puts money into the balance' do
-      account.deposit(500, "09/22/17")
+      account.deposit(500, '09/22/17')
       expect(account.balance).to eq(500)
     end
-    # it "adds a date to the trainsaction" do
-    #   account.deposit(400, "09/22/17")
-    #   expect(info.date).to eq("09/22/17")
-    # end
+    it 'adds a date to the trainsaction' do
+      account.deposit(400, '09/22/17')
+      expect(account.transaction_history[0].date).to eq('09/22/17')
+    end
   end
 
   context '#withdrawl' do
     it 'puts money into the balance' do
-      account.deposit(500, "09/22/17")
-      account.withdrawl(250, "09/22/17")
+      account.deposit(500, '09/22/17')
+      account.withdrawl(250, '09/22/17')
       expect(account.balance).to eq(250)
     end
   end
 
   context '#show_balance' do
     it 'shows the current balance' do
-      account.deposit(3000, "09/22/17")
-      account.withdrawl(2500, "09/22/17")
+      account.deposit(3000, '09/22/17')
+      account.withdrawl(2500, '09/22/17')
       expect(account.balance).to eq(500)
+    end
+  end
+
+  context '#current_transaction' do
+    it 'shows the latest transaction' do
+      account.deposit(200, '09/22/17')
+      account.deposit(400, '10/22/17')
+      expect(account.transaction_history[1].credit).to eq(400)
+      expect(account.transaction_history[1].date).to eq('10/22/17')
     end
   end
 
   context '#transaction_history' do
     it 'puts a transaction history into the transaction history array when you make a deposit' do
-      account.deposit(3000, "09/22/17")
+      account.deposit(3000, '09/22/17')
       expect(account.transaction_history[0].credit).to eq(3000)
       expect(account.transaction_history.length).to eq(1)
     end
