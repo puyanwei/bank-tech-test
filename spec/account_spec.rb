f0b9044969fcc4e04e9c9d551e4fc7dcf2b8d219
pyan83 <puyanwei@gmail.com>
2017-10-03 19:12:25 +0100

    got tests passing with log as name of hash table

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -38,8 +38,8 @@ RSpec.describe Account do
 
   context '#add_to_history' do
     it 'adds the current transaction the transaction history' do
-      account.deposit(200, '09/22/17')
-      account.deposit(400, '10/22/17')
+      account.deposit('09/22/17', 200)
+      account.deposit('10/22/17', 400)
       expect(account.transaction_history[1].log[:credit]).to eq(400)
       expect(account.transaction_history[1].log[:date]).to eq('10/22/17')
     end
@@ -47,7 +47,7 @@ RSpec.describe Account do
 
   context '#transaction_history' do
     it 'puts a transaction history into the transaction history array when you make a deposit' do
-      account.deposit(3000, '09/22/17')
+      account.deposit('09/22/17', 3000)
       expect(account.transaction_history[0].log[:credit]).to eq(3000)
       expect(account.transaction_history.length).to eq(1)
     end
