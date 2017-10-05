20a0a00e441312021d14ce9bbd4f36b074089d3b
pyan83 <puyanwei@gmail.com>
2017-10-05 16:28:03 +0100

    made an error checking method to contain the creation of a new error instance in the Withdrawal class

--- lib/withdrawal.rb
+++ lib/withdrawal.rb
@@ -1,11 +1,15 @@
 class Withdrawal
   attr_accessor :date, :credit, :debit, :balance
 
-  def initialize(date, credit, balance, error = Error.new)
+  def initialize(date, credit, balance)
     @date = date
     @credit = credit
     @debit = 0
     @balance = balance
+  end
+
+  def error_check
+    error = Error.new
     error.insufficient_funds(debit, credit)
   end
 end
